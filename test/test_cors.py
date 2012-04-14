99e2e6c3efa229c3eff1d7f1800370a4396ef7de
Chris Dent <chris.dent@gmail.com>
2012-04-14 17:24:04 +0100

    Implementation of CORS responses and preflight request handling.
    See the pydoc and README for some more information. This implementation
    is based on reading: http://www.html5rocks.com/en/tutorials/cors/
    
    Further testing required before we know if this is any good.

---
+++ test/test_cors.py
@@ -0,0 +1,95 @@
+"""
+Test cors based on docs at
+http://www.html5rocks.com/en/tutorials/cors/
+"""
+
+
+from wsgi_intercept import httplib2_intercept
+import wsgi_intercept
+import httplib2
+
+from tiddlyweb.config import config
+from tiddlyweb.web.serve import load_app
+
+
+def setup_module(module):
+    app = load_app()
+    def app_fn():
+        return app
+
+    httplib2_intercept.install()
+    wsgi_intercept.add_wsgi_intercept('0.0.0.0', 8080, app_fn)
+
+    module.http = httplib2.Http()
+
+
+def test_simple_get_no_cors():
+    response, content = http.request('http://0.0.0.0:8080/')
+    assert response['status'] == '200'
+    assert '/bags' in content
+
+def test_simple_get_cors():
+    response, content = http.request('http://0.0.0.0:8080/',
+            headers = {'Origin': 'http://example.com'})
+    assert response['status'] == '200'
+    assert '/bags' in content
+    assert response['access-control-allow-origin'] == '*'
+    assert response['access-control-expose-headers'] == 'ETag'
+
+def test_simple_get_match_origin():
+    config['cors.match_origin'] = True
+    response, content = http.request('http://0.0.0.0:8080/',
+            headers = {'Origin': 'http://example.com'})
+    assert response['status'] == '200'
+    assert '/bags' in content
+    assert response['access-control-allow-origin'] == 'http://example.com'
+    assert response['access-control-expose-headers'] == 'ETag'
+    config['cors.match_origin'] = False
+
+def test_simple_get_allow_auth():
+    config['cors.allow_creds'] = True
+    response, content = http.request('http://0.0.0.0:8080/',
+            headers = {'Origin': 'http://example.com'})
+    assert response['status'] == '200'
+    assert '/bags' in content
+    assert response['access-control-allow-origin'] == '*'
+    assert response['access-control-expose-headers'] == 'ETag'
+    assert response['access-control-allow-credentials'] == 'true'
+    config['cors.allow_creds'] = False
+
+def test_simple_custom_exposed_headers():
+    config['cors.exposed_headers'] = ['Frank', 'Barney']
+    response, content = http.request('http://0.0.0.0:8080/',
+            headers = {'Origin': 'http://example.com'})
+    assert response['status'] == '200'
+    assert '/bags' in content
+    assert response['access-control-allow-origin'] == '*'
+    assert response['access-control-expose-headers'] == 'ETag, Frank, Barney'
+    del config['cors.exposed_headers']
+
+def test_not_simple_no_auth():
+    response, content = http.request('http://0.0.0.0:8080/bags/franny',
+            method = 'OPTIONS',
+            headers = {'Origin': 'http://example.com',
+                'Access-Control-Request-Method': 'DELETE',
+                'Access-Control-Request-Headers': 'X-Custom'})
+    assert response['status'] == '200'
+    assert response['access-control-allow-origin'] == 'http://example.com'
+    assert response['access-control-allow-methods'] == 'DELETE, GET, PUT'
+    assert response['access-control-allow-headers'] == 'ETag, X-Custom'
+    assert response['access-control-max-age'] == '600'
+
+def test_not_simple_auth():
+    config['cors.allow_creds'] = True
+    response, content = http.request('http://0.0.0.0:8080/bags/franny',
+            method = 'OPTIONS',
+            headers = {'Origin': 'http://example.com',
+                'Access-Control-Request-Method': 'DELETE',
+                'Access-Control-Request-Headers': 'X-Custom'})
+    assert response['status'] == '200'
+    assert response['access-control-allow-origin'] == 'http://example.com'
+    assert response['access-control-allow-methods'] == 'DELETE, GET, PUT'
+    assert response['access-control-allow-headers'] == 'ETag, X-Custom'
+    assert response['access-control-max-age'] == '600'
+    assert response['access-control-allow-credentials'] == 'true'
+    config['cors.allow_creds'] = False
--- tiddlywebplugins/cors.py
+++ tiddlywebplugins/cors.py
@@ -1,11 +1,132 @@
 """
-CORS preflight OPTIONS checks for TiddlyWeb.
+CORS handling for TiddlyWeb.
 """
 
+from tiddlyweb.web.wsgi import EncodeUTF8
+
+DEFAULT_EXPOSED_HEADERS = ['ETag']
+DEFAULT_CORS_CACHE_AGE = '600'  # 10 minutes
+
 class PreFlightCheck(object):
-    pass
+    """
+    Handle incoming pre-flight OPTIONS requests.
+
+    This middleware is only enabled if config['cors.enable_non_simple']
+    is True. The default is False.
+
+    When an OPTIONS request is made, the response will 404 if the route
+    is not available. The methods header will only respond with those
+    methods which are actually valid for the requested resource. Both of
+    these things are done by called select() on the selector app.
+    """
+    def __init__(self, application):
+        self.application = application
+
+    def __call__(self, environ, start_response):
+
+        if ('HTTP_ORIGIN' in environ
+                and environ['REQUEST_METHOD'] == 'OPTIONS'):
+
+            selector = environ['tiddlyweb.config']['selector']
+
+            app, _, methods, _ = selector.select(
+                    environ['PATH_INFO'], environ['REQUEST_METHOD'])
+
+            if methods:  # There are methods on this route
+                config = environ.get('tiddlyweb.config', {})
+
+                allowed_origin = environ['HTTP_ORIGIN']
+                exposed_headers = ', '.join(DEFAULT_EXPOSED_HEADERS)
+                extra_headers = environ.get(
+                        'HTTP_ACCESS_CONTROL_REQUEST_HEADERS', None) 
+                if extra_headers:
+                    exposed_headers = exposed_headers + ', %s' % extra_headers
+
+                headers = [('Access-Control-Allow-Origin', allowed_origin),
+                        ('Access-Control-Allow-Headers', exposed_headers)]
+
+                if config.get('cors.allow_creds', False):
+                    headers.append(('Access-Control-Allow-Credentials', 'true'))
+
+                headers.append(('Access-Control-Max-Age',
+                    DEFAULT_CORS_CACHE_AGE))
+
+                headers.append(('Access-Control-Allow-Methods',
+                    ', '.join(sorted(methods))))
+
+                start_response('200 OK', headers)
+                return []
+            else:
+                return app(environ, start_response)
+        else:
+            return self.application(environ, start_response)
+
+class CORSResponse(object):
+    """
+    Send correct headers in response to a (non-preflight) request
+    that provided a CORS 'Origin' header.
+
+    By default if Origin was in the header, then
+
+        Access-Control-Allow-Origin: *
+        Access-Control-Expose-Headers: ETag
+
+    are sent in the response.
+
+    For the time being these responses are sent on any request
+    method with any status code.
+
+    Config settings allow more granular control:
+
+    If 'cors.match_origin' is True, then the value of the Origin
+    header will be the value of the Access-Control-Allow-Origin header.
+
+    If 'cors.allow_creds' is True, then the
+    Access-Control-Allow-Credentials header will be sent with a value
+    of 'true', otherwise it will not be sent.
+
+    If 'cors.exposed_headers' is set, its should be a list of strings
+    representing header names which are appended to the default
+    Access-Control-Expose-Headers: ETag
+    """
+
+    def __init__(self, application):
+        self.application = application
+
+    def __call__(self, environ, start_response):
+
+        def replacement_start_response(status, headers, exc_info=None):
+
+            config = environ.get('tiddlyweb.config', {})
+
+            allowed_origin = '*'
+            if config.get('cors.match_origin', False):
+                allowed_origin = environ['HTTP_ORIGIN']
+
+            exposed_headers = ', '.join(DEFAULT_EXPOSED_HEADERS
+                    + config.get('cors.exposed_headers', []))
+
+            headers.extend([('Access-Control-Allow-Origin', allowed_origin),
+                    ('Access-Control-Expose-Headers', exposed_headers)])
+
+            if config.get('cors.allow_creds', False):
+                headers.append(('Access-Control-Allow-Credentials', 'true'))
+
+            return start_response(status, headers, exc_info)
+
+        if 'HTTP_ORIGIN' in environ and environ['REQUEST_METHOD'] != 'OPTIONS':
+            return self.application(environ, replacement_start_response)
+        else:
+            return self.application(environ, start_response)
+
 
 def init(config):
     if 'selector' in config:
-        if PreFlightCheck not in config['server_request_filters']:
-            config['server_request_filters'].append(PreFlightCheck)
+        if config.get('cors.enable_non_simple', False):
+            if PreFlightCheck not in config['server_request_filters']:
+                config['server_request_filters'].append(PreFlightCheck)
+        if CORSResponse not in config['server_response_filters']:
+            config['server_response_filters'].insert(
+                    config['server_response_filters']
+                    .index(EncodeUTF8) + 1, CORSResponse)
+
